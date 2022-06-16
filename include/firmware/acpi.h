ce978881c513e0899742986964ec9b838e09ea85
Louis Leseur <louis.leseur@gmail.com>
2022-06-16 08:06:21 +0200

    Add missing doc for firmware/acpi.h and irq.h

--- include/firmware/acpi.h
+++ include/firmware/acpi.h
@@ -1,9 +1,18 @@
+/**
+ * @file firmware/acpi.h
+ * @brief Advanced Configuration and Power Interface
+ */
+
 #ifndef FIRMWARE_ACPI_H
 #define FIRMWARE_ACPI_H
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * @brief Register RSDP address
+ * @param paddr Physical address of RSDP
+ */
 void acpi_register_rsdp(void * paddr);
 
 #ifdef __cplusplus
--- include/irq.h
+++ include/irq.h
@@ -1,13 +1,41 @@
+/**
+ * @file irq.h
+ * @brief Interrupt management
+ */
+
 #ifndef IRQ_H
 #define IRQ_H
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * @brief Enable IRQs
+ * @return errno
+ */
 int irq_enable(void);
+
+/**
+ * @brief Disable IRQs
+ */
 void irq_disable(void);
+
+/**
+ * @brief Mask an IRQ
+ * @param irq IRQ number
+ */
 void irq_mask(unsigned irq);
+
+/**
+ * @brief Unmask an IRQ
+ * @param irq IRQ number
+ */
 void irq_unmask(unsigned irq);
+
+/**
+ * @brief Send an end-of-interrupt
+ * @param irq IRQ number
+ */
 void irq_eoi(unsigned irq);
 
 #ifdef __cplusplus
